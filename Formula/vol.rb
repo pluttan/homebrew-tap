53bf4e7c9e45322bf53a1e7d0b584897defe801d
Andrew Plutto <pluttan@ya.ru>
2026-01-06 21:31:09 +0300

    Fix SHA256

--- Formula/vol.rb
+++ Formula/vol.rb
@@ -2,7 +2,7 @@ class Vol < Formula
   desc "Universal build tool with beautiful terminal output"
   homepage "https://github.com/pluttan/volumes"
   url "https://github.com/pluttan/volumes/releases/download/v2.0.0/vol"
-  sha256 "PLACEHOLDER_SHA256"
+  sha256 "b282b4dd6423a03c97e45adb8f12a5858d52218f9df150a7f860e0021f98cbed"
   license "MIT"
   version "2.0.0"
 
