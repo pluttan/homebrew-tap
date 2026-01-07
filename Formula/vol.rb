6a7bc8a73cad442dc3491e98b0a3df8dc2b73e35
Andrew Plutto <pluttan@ya.ru>
2026-01-07 11:37:08 +0300

    vol 2.0.19

--- Formula/vol.rb
+++ Formula/vol.rb
@@ -6,8 +6,35 @@ class Vol < Formula
   license "MIT"
   version "2.0.19"
 
+  resource "zsh_completion" do
+    url "https://github.com/pluttan/volumes/releases/download/v2.0.19/_vol"
+    sha256 "777cf012e308916efafacf2659eebf24f7d9c0a30f47f517631a51897978cffb"
+  end
+
+  resource "bash_completion" do
+    url "https://github.com/pluttan/volumes/releases/download/v2.0.19/vol.bash"
+    sha256 "273cf4d4f942a6b7b568af2241077ddc38987a692fbebb5187118c4438441278"
+  end
+
+  resource "fish_completion" do
+    url "https://github.com/pluttan/volumes/releases/download/v2.0.19/vol.fish"
+    sha256 "3006a4fab6cd9920173ede24b39eba383500cffe410019c3a6de1b99a8c8ccfd"
+  end
+
   def install
     bin.install "vol"
+    
+    resource("zsh_completion").stage do
+      zsh_completion.install "_vol"
+    end
+    
+    resource("bash_completion").stage do
+      bash_completion.install "vol.bash" => "vol"
+    end
+    
+    resource("fish_completion").stage do
+      fish_completion.install "vol.fish"
+    end
   end
 
   test do
