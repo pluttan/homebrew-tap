02dde88a43646e2a3cc69d9f08773355f57af7f6
Andrew Plutto <pluttan@ya.ru>
2026-01-07 03:24:33 +0300

    vol 2.0.17

--- Formula/vol.rb
+++ Formula/vol.rb
@@ -2,12 +2,39 @@ class Vol < Formula
   desc "Universal build tool with beautiful terminal output"
   homepage "https://github.com/pluttan/volumes"
   url "https://github.com/pluttan/volumes/releases/download/v2.0.18/vol"
-  sha256 "02fcd77be5dcd82f4148142956fead9914fa8ac572f5ed630010339a0eff4698"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   license "MIT"
   version "2.0.18"
 
+  resource "zsh_completion" do
+    url "https://github.com/pluttan/volumes/releases/download/v2.0.18/_vol"
+    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  end
+
+  resource "bash_completion" do
+    url "https://github.com/pluttan/volumes/releases/download/v2.0.18/vol.bash"
+    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  end
+
+  resource "fish_completion" do
+    url "https://github.com/pluttan/volumes/releases/download/v2.0.18/vol.fish"
+    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  end
+
   def install
     bin.install "vol"
+    
+    resource("zsh_completion").stage do
+      zsh_completion.install "_vol"
+    end
+    
+    resource("bash_completion").stage do
+      bash_completion.install "vol.bash" => "vol"
+    end
+    
+    resource("fish_completion").stage do
+      fish_completion.install "vol.fish"
+    end
   end
 
   test do
